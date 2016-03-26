06591fdf82c61719a1f2441205f1358d9d9bfb52
ajax chai <ajaxchai@gmail.com>
2016-03-26 14:05:37 +0800

    add xstream and dom4j libs

--- src/com/control/service/CoreService.java
+++ src/com/control/service/CoreService.java
@@ -4,11 +4,21 @@ import java.io.InputStream;
 import java.io.Writer;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
 import com.control.meta.TextMessage;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
 
 public class CoreService {
 	/**
